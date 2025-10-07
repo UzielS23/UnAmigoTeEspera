c94243f3d9c4ffab0d1126f8c0188bed56693a9d
rkive250 <rkive250@gmail.com>
2025-10-07 02:14:05 -0500

    cambiosdashboard
    dashboard

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('dashboard.html')
 
+@app.route('/dashboard-graficas')
+def dashboard_graficas():
+    return render_template('dashboardgraficas.html')
 
 @app.route('/service-worker.js')
 def service_worker():
@@ -17,7 +20,32 @@ def service_worker():
 def manifest():
     return send_from_directory('static', 'manifest.json', mimetype='application/manifest+json')
 
+@app.route("/padrinos")
+def padrinos():
+    return render_template("padrinos.html")
 
+@app.route("/apoyos")
+def apoyos():
+    return render_template("apoyos.html")
+
+@app.route("/mascotas")
+def mascotas():
+    return render_template("mascotas.html")
+
+@app.route("/refugios")
+def refugios():
+    return render_template("refugios.html")
+
+@app.route("/notificaciones")
+def notificaciones():
+    return render_template("notificaciones.html")
+
+@app.route("/partials/<name>")
+def partials(name):
+   
+    return render_template(f"{name}.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
